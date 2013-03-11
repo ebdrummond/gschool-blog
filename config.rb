6e23f1d6f8283b5e5b0a736650b54b4e06f05dc2
erindrummond <e.b.drummond@gmail.com>
2013-03-11 16:39:55 -0600

    HTML and CSS reading group 1

--- config.rb
+++ config.rb
@@ -76,6 +76,14 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :syntax,
+              :linenos => 'inline',
+              :anchorlinenos => true,
+              :linenostart => 2
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -98,11 +106,4 @@ configure :build do
   # Or use a different image path
   # set :http_path, "/Content/images/"
 
-  set :markdown_engine, :redcarpet
-  set :markdown, :fenced_code_blocks => true, :smartypants => true
-
-  activate :syntax,
-                :linenos => 'inline',
-                :anchorlinenos => true,
-                :linenostart => 2
 end
